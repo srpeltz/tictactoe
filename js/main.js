9a798a76f24d8865284059d9bf91cc1c6c20180f
steven peltzer <srpeltzer@yahoo.com>
2017-01-28 12:50:53 -0800

    js basics laid out

--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
 console.log("You're Connected!")
 
+
+////// GLOBAL VARIABLES ////////
+///////////////////////////////
 var board = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+  [box1, box2, box3],
+  [box4, box5, box6],
+  [box7, box8, box9],
 ]
+//make variables for  document.getElementById for each square so it can be called later in the code.//
+var box1 = document.getElementById('box1').addEventListener('click', function() {
+
+})
+var box2 = document.getElementById('box2')
+var box3 = document.getElementById('box3')
+var box4 = document.getElementById('box4')
+var box5 = document.getElementById('box5')
+var box6 = document.getElementById('box6')
+var box7 = document.getElementById('box7')
+var box8 = document.getElementById('box8')
+var box9 = document.getElementById('box9')
+
+var player1 = player1
+var player2 = player2
+
+
+//////FUNCTIONS//////
+////////////////////
+function()
+
+//player 1 is chosen to select a square.
+function
+
+//player 1 clicks a square and that square is assigned a color
+//player 2 is chosen to select a square
+//player 2 clicks a square and that square is assigned a color
+
+//if player one gets three in a row then he is the winner
+//if player two gets three in a row then he is the winner
+
+//if a cats game pop alert and automatically reset. (Copy code from dog lab)
