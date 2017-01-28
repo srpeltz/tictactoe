e00e1100db95ba48b53790d50a4310266834848e
steven peltzer <srpeltzer@yahoo.com>
2017-01-28 15:12:56 -0800

    alternating Xs and Os

--- js/main.js
+++ js/main.js
@@ -10,89 +10,39 @@ var board = [
 ]
 //make variables for  document.getElementById for each square so it can be called later in the code.//
 var playerX = true
-
-function altTurn() {
-  if(playerX) {
-    playerX = false
-  }
-}
-
 var imageX = 'http://www.iconsplace.com/download/black-letter-x-512.jpg'
-
 var imageO = 'http://cdn3.volusion.com/joqnj.zjnrj/v/vspfiles/photos/REX00LB1O-2.jpg'
 
-document.getElementById('box1').addEventListener('click', function() {
+function altTurn() {
   if (playerX) {
-    this.style.backgroundImage = 'url(' + imageX + ')'
-  } else {
-    this.style.backgroundImage = 'url(' + imageO + ')'
+    this.style.backgroundImage = 'url(' + imageX + ')';
+    playerX=false
+  }else {
+    this.style.backgroundImage = 'url(' + imageO + ')';
+    playerX=true
   }
-})
-document.getElementById('box2').addEventListener('click', function() {
-  if (playerX) {
-    this.style.backgroundImage = 'url(' + imageX + ')'
-  } else {
-    this.style.backgroundImage = 'url(' + imageO + ')'
-  }
-})
-document.getElementById('box3').addEventListener('click', function() {
-  if (playerX) {
-    this.style.backgroundImage = 'url(' + imageX + ')'
-  } else {
-    this.style.backgroundImage = 'url(' + imageO + ')'
-  }
-})
-document.getElementById('box4').addEventListener('click', function() {
-  if (playerX) {
-    this.style.backgroundImage = 'url(' + imageX + ')'
-  } else {
-    this.style.backgroundImage = 'url(' + imageO + ')'
-  }
-})
-document.getElementById('box5').addEventListener('click', function() {
-  if (playerX) {
-    this.style.backgroundImage = 'url(' + imageX + ')'
-  } else {
-    this.style.backgroundImage = 'url(' + imageO + ')'
-  }
-})
-document.getElementById('box6').addEventListener('click', function() {
-  if (playerX) {
-    this.style.backgroundImage = 'url(' + imageX + ')'
-  } else {
-    this.style.backgroundImage = 'url(' + imageO + ')'
-  }
-})
-document.getElementById('box7').addEventListener('click', function() {
-  if (playerX) {
-    this.style.backgroundImage = 'url(' + imageX + ')'
-  } else {
-    this.style.backgroundImage = 'url(' + imageO + ')'
-  }
-})
-document.getElementById('box8').addEventListener('click', function() {
-  if (playerX) {
-    this.style.backgroundImage = 'url(' + imageX + ')'
-  } else {
-    this.style.backgroundImage = 'url(' + imageO + ')'
-  }
-})
-document.getElementById('box9').addEventListener('click', function() {
-  if (playerX) {
-    this.style.backgroundImage = 'url(' + imageX + ')'
-  } else {
-    this.style.backgroundImage = 'url(' + imageO + ')'
-  }
-})
 
-var box2 = document.getElementById('box2')
-var box3 = document.getElementById('box3')
-var box4 = document.getElementById('box4')
-var box5 = document.getElementById('box5')
-var box6 = document.getElementById('box6')
-var box7 = document.getElementById('box7')
-var box8 = document.getElementById('box8')
-var box9 = document.getElementById('box9')
+  // playerX ? this.style.backgroundImage = 'url(' + imageX + ')' playerX=false :   this.style.backgroundImage = 'url(' + imageO + ')' playerX=true)
+}
+
+document.getElementById('box1').addEventListener('click', altTurn);
+document.getElementById('box2').addEventListener('click', altTurn);
+document.getElementById('box3').addEventListener('click', altTurn);
+document.getElementById('box4').addEventListener('click', altTurn);
+document.getElementById('box5').addEventListener('click', altTurn);
+document.getElementById('box6').addEventListener('click', altTurn);
+document.getElementById('box7').addEventListener('click', altTurn);
+document.getElementById('box8').addEventListener('click', altTurn);
+document.getElementById('box9').addEventListener('click', altTurn);
+
+// var box2 = document.getElementById('box2')
+// var box3 = document.getElementById('box3')
+// var box4 = document.getElementById('box4')
+// var box5 = document.getElementById('box5')
+// var box6 = document.getElementById('box6')
+// var box7 = document.getElementById('box7')
+// var box8 = document.getElementById('box8')
+// var box9 = document.getElementById('box9')
 
 
 
