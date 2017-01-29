75fcc3dbd4adf754e481d9776f9bf839a75486c0
steven peltzer <srpeltzer@yahoo.com>
2017-01-28 16:35:25 -0800

    assigned value to Xs and Os

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 console.log("You're Connected!")
 
-
 ////// GLOBAL VARIABLES ////////
 ///////////////////////////////
 var board = [
@@ -8,7 +7,7 @@ var board = [
   [box4, box5, box6],
   [box7, box8, box9],
 ]
-//make variables for  document.getElementById for each square so it can be called later in the code.//
+
 var playerX = true
 var imageX = 'http://www.iconsplace.com/download/black-letter-x-512.jpg'
 var imageO = 'http://cdn3.volusion.com/joqnj.zjnrj/v/vspfiles/photos/REX00LB1O-2.jpg'
@@ -16,45 +15,34 @@ var imageO = 'http://cdn3.volusion.com/joqnj.zjnrj/v/vspfiles/photos/REX00LB1O-2
 function altTurn() {
   if (playerX) {
     this.style.backgroundImage = 'url(' + imageX + ')';
-    playerX=false
+    this.setAttribute('box-value', 1);
+    playerX=false;
   }else {
     this.style.backgroundImage = 'url(' + imageO + ')';
+    this.setAttribute('box-value', 2);
     playerX=true
   }
-
-  // playerX ? this.style.backgroundImage = 'url(' + imageX + ')' playerX=false :   this.style.backgroundImage = 'url(' + imageO + ')' playerX=true)
 }
 
-document.getElementById('box1').addEventListener('click', altTurn);
-document.getElementById('box2').addEventListener('click', altTurn);
-document.getElementById('box3').addEventListener('click', altTurn);
-document.getElementById('box4').addEventListener('click', altTurn);
-document.getElementById('box5').addEventListener('click', altTurn);
-document.getElementById('box6').addEventListener('click', altTurn);
-document.getElementById('box7').addEventListener('click', altTurn);
-document.getElementById('box8').addEventListener('click', altTurn);
-document.getElementById('box9').addEventListener('click', altTurn);
+function winner(){
+    if (board[0][0].style.backgroundImage == imageX && board[0,1].style.backgroundImage == imageX && board[0][2].style.backgroundImage == imageX) {
+      alert('X WINS FAM!')
+    }
+}
 
-// var box2 = document.getElementById('box2')
-// var box3 = document.getElementById('box3')
-// var box4 = document.getElementById('box4')
-// var box5 = document.getElementById('box5')
-// var box6 = document.getElementById('box6')
-// var box7 = document.getElementById('box7')
-// var box8 = document.getElementById('box8')
-// var box9 = document.getElementById('box9')
-
-
+var box1 = document.getElementById('box1').addEventListener('click', altTurn);
+var box2 = document.getElementById('box2').addEventListener('click', altTurn);
+var box3 = document.getElementById('box3').addEventListener('click', altTurn);
+var box4 = document.getElementById('box4').addEventListener('click', altTurn);
+var box5 = document.getElementById('box5').addEventListener('click', altTurn);
+var box6 = document.getElementById('box6').addEventListener('click', altTurn);
+var box7 = document.getElementById('box7').addEventListener('click', altTurn);
+var box8 = document.getElementById('box8').addEventListener('click', altTurn);
+var box9 = document.getElementById('box9').addEventListener('click', altTurn);
 
 //////FUNCTIONS//////
 ////////////////////
 
-//player 1 is chosen to select a square.
-
-
-//player 1 clicks a square and that square is assigned a color
-//player 2 is chosen to select a square
-//player 2 clicks a square and that square is assigned a color
 
 //if player one gets three in a row then he is the winner
 //if player two gets three in a row then he is the winner
