927bd5b17922fefe97eb65f3174ebc019b00124b
steven peltzer <srpeltzer@yahoo.com>
2017-01-29 20:49:21 -0800

    updated single click feature

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ console.log("You're Connected!")
 
 ////// GLOBAL VARIABLES ////////
 ///////////////////////////////
-var board = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
+var board = ['box1', 'box2', 'box3', 'box4', 'box5', 'box6', 'box7', 'box8', 'box9']
 
 var playerX = true
 
@@ -25,24 +25,22 @@ function altTurn(e) {
   if (playerX) {
     this.style.backgroundImage = 'url(' + imageX + ')';
     this.setAttribute('box-value', 1);
-    e.target.removeEventListener(e.type, arguments.callee)
+    e.target.removeEventListener(e.type, arguments.callee);
     playerX=false;
   }else {
     this.style.backgroundImage = 'url(' + imageO + ')';
     this.setAttribute('box-value', 2);
-    e.target.removeEventListener(e.type, arguments.callee)
-    playerX=true
+    e.target.removeEventListener(e.type, arguments.callee);
+    playerX=true;
   }
 }
 
-
-
-// function winner(){
-//   //nodeValue possible??
-//     if ((board[0].getAttribute('box-value') == "2") && (board[1].getAttribute('box-value') == "2") && (board[2].getAttribute('box-value') == "2")) {
-//       alert('X WINS FAM!')
-//     }
-// }
+function winner(){
+  //nodeValue possible??
+    if ((board[0].document.querySelector('[box-value]') === "1") && (board[1].document.querySelector('[box-value]') === "1") && (board[2].document.querySelector('[box-value]') === "1")) {
+      alert('X FTW!')
+    }
+}
 
 //if player one gets three in a row then he is the winner
 //if player two gets three in a row then he is the winner
